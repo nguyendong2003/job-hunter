ac614cbe2523f5b9fad98c067244beda7c3871ca
nguyendong2003 <dongcoi14122003@gmail.com>
2024-07-31 18:02:48 +0700

    fix cors error when connect frontend vs backend and fix get mapping (response String) in hello controller

--- src/main/java/vn/nguyendong/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/nguyendong/jobhunter/config/SecurityConfiguration.java
@@ -18,8 +18,6 @@ import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
-import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.nimbusds.jose.jwk.source.ImmutableSecret;
@@ -99,6 +97,7 @@ public class SecurityConfiguration {
             CustomAuthenticationEntryPoint customAuthenticationEntryPoint) throws Exception {
         http
                 .csrf(c -> c.disable())
+                .cors(Customizer.withDefaults()) // cấu hình cors mặc định
                 .authorizeHttpRequests((authz) -> authz
                         .requestMatchers("/", "/login").permitAll()
                         .anyRequest().authenticated())
--- src/main/java/vn/nguyendong/jobhunter/util/SecurityUtil.java
+++ src/main/java/vn/nguyendong/jobhunter/util/SecurityUtil.java
@@ -3,9 +3,6 @@ package vn.nguyendong.jobhunter.util;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
